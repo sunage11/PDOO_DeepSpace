ca1add650378163886a637fcf0b862d0f561e975
Soledad Ruiz <solerugo@hotmail.com>
2022-03-25 10:00:38 +0100

    Damage unfinished

--- Deepspace_Ruby/P2/Damage.rb
+++ Deepspace_Ruby/P2/Damage.rb
@@ -12,28 +12,33 @@ module DeepSpace
 
         attr_reader :nShields, :nWeapons, :weapons
         
-        # Constructor
+        # nWeapons constructor -> numeric damage
         def newNumericWeapons (w, s)
             @nShields = s 
             @nWeapons = w
             @weapons=Array.new()
         end
 
-        #Constructor 
+        # WeaponType constructor -> specific damage 
         def newSpecificWeapons(w,s)
             @weapons=Array.new(w)
             @nShields= s
-            @nWeapons= @weapons.size
+            @nWeapons= -1
             
         end
-        # otro
 
-        #otro
+        # Copy constructor
+        def newCopy(d)
+            return d.clone
+        end
 
+
+        # builds a new DamageToUI object from *this
         def getUIversion (d)
             DamageToUI.new(self)
         end
-
+        
+        # ----
         def arrayContainsType (w, t)
             cont=true
             i=0
@@ -52,16 +57,32 @@ module DeepSpace
 
         end
 
+        # returns a an adjusted version of *this. It takes into consideration
+        # the parameters w ans sb in order to modify the output value so it does not
+        # imply losing weapons or shields that are nos specified in w or sb
         def adjuste (w, s)
         end
 
+        # If *this has w.getType() in the array weapons, it deletes that element
+        # of the array. In other case, it decrements nWeapons in one unit
         def discardWeapon (w)
+            if (@weapons.delete(w) == nil)
+                if (@nWeapons>0)
+                    @nWeapons -= 1
+                end
+            end
         end
 
+        # it decrements nShields in one unit
         def discardShieldBooster
+            if (@nShields>0)
+                @nShields -= 1
+            end
         end
 
+        # returns true if *this does not imply any accessory loss
         def hasNoEffect
+            return ((@nWeapons==0) && (@nShields==0) && (@weapons.empty?))
         end
 
         #toString
