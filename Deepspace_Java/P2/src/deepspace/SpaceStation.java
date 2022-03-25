a1ae5a7b6b8c56d3930442df2570cf35391e5d7b
clarabolivar <cbolivarpelaez@gmail.com>
2022-03-25 11:31:33 +0100

    SpaceStation

--- Deepspace_Java/P2/src/deepspace/SpaceStation.java
+++ Deepspace_Java/P2/src/deepspace/SpaceStation.java
@@ -6,6 +6,7 @@
 package deepspace;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -67,35 +68,53 @@ public class SpaceStation {
     }
     
     /**
-    * @brief Copy constructor
-    * @param d another Damage instance
+    * @brief It eliminates all mounted weapons and boosters that do not have 
+    * any use left.
     */
     void cleanUpMountedItems () {
         
+        Iterator<Weapon> it = weapons.iterator();
+        Iterator<ShieldBooster> it2 = shieldBoosters.iterator();
+        
+        while(it.hasNext()){
+            Weapon aux = it.next();
+            if(aux.getUses() == 0)
+                weapons.remove(aux);
+        }
+        
+        while(it2.hasNext()){
+            ShieldBooster aux2 = it2.next();
+            if(aux2.getUses() == 0)
+                shieldBoosters.remove(aux2);
+        }
+        
+        
     }
     
     /**
-    * @brief Copy constructor
-    * @param d another Damage instance
+    * @brief Set the reference of the hangar to null to indicate that it 
+    * is not available.
     */
     void discardHangar () {
-        
+        hangar = null;
     }
     
     /**
-    * @brief Copy constructor
-    * @param d another Damage instance
+    * @brief 
+    * @param 
     */
     void discardShieldBooster (int i) {
-        
+        //Se hace en la práctica 3
     }
     
     /**
-    * @brief Copy constructor
-    * @param d another Damage instance
+    * @brief If a hangar is available, it is requested to discard the booster 
+    * with index i.
+    * @param i index
     */
     void discardShieldBoosterInHangar (int i) {
-        
+        if(hangar!=null)
+            hangar.removeShieldBooster(i);
     }
     
     /**
@@ -103,14 +122,17 @@ public class SpaceStation {
     * @param d another Damage instance
     */
     void discardWeapon (int i) {
-        
+        //Se hace en la práctica 3
     }
     
     /**
-    * @brief Copy constructor
-    * @param d another Damage instance
+    * @brief If a hangar is available, it is requested to discard the weapon 
+    * with index i.
+    * @param i index
     */
     void discardWeaponInHangar (int i) {
+        if(hangar!=null)
+            hangar.removeWeapon(i);
         
     }
     
@@ -119,7 +141,7 @@ public class SpaceStation {
     * @param d another Damage instance
     */
     float fire () {
-        
+        //Se hace en la práctica 3
     }
     
     /**
@@ -148,7 +170,7 @@ public class SpaceStation {
     
     /**
     * @brief Getter with package visibility
-    * @return hangar instance
+    * @return Hangar instance
     */
     Hangar getHangar () {
         return (hangar);
@@ -164,50 +186,43 @@ public class SpaceStation {
     
     /**
     * @brief Getter with package visibility
-    * @param d another Damage instance
+    * @return nMedals value 
     */
     int getNMedals () {
-        
+        return(nMedals);
     }
     
     /**
     * @brief Getter with package visibility
-    * @param d another Damage instance
+    * @return pendingDamage instance
     */
     Damage getPendingDamage () {
-        
+        return (pendingDamage);
     }
     
     /**
     * @brief Getter with package visibility
-    * @param d another Damage instance
+    * @return collection of ShieldBoosters
     */
-    ShieldBooster[] getShieldBooster () {
-        
+    ArrayList<ShieldBooster> getShieldBooster () {
+        return(shieldBoosters);
     }
     
     /**
     * @brief Getter with package visibility
-    * @param d another Damage instance
+    * @return shieldPower instance
     */
     float getShieldPower () {
-        
+        return (shieldPower);
     }
     
-    /**
-    * @brief Getter with package visibility
-    * @param d another Damage instance
-    */
-    float getPower () {
-        
-    }
     
     /**
     * @brief Getter with package visibility
     * @param d another Damage instance
     */
     float getSpeed () {
-        
+        return(fuelUnits/MAXFUEL);
     }
     
     /**
@@ -219,27 +234,37 @@ public class SpaceStation {
     }
     
     /**
-    * @brief builds a new WeaponToUI object from *this
-    * @return WeaponToUI
+    * @brief Getter with package visibility
+    * @return collection of Weapons
     */
-    Weapon[] getWeapons () {
-        
+    ArrayList<Weapon> getWeapons () {
+        return (weapons);
     }
     
     /**
-    * @brief builds a new WeaponToUI object from *this
-    * @return WeaponToUI
+    * @brief It trys to mount the booster with the index i inside the hangar. If 
+    * a hangar is available, it removes the booster located in that position 
+    * and if this operation is successful, it is added to the collection of
+    * boosters in use.
+    * @param i index
     */
     void mountShieldBooster (int i) {
-        
+        ShieldBooster aux;
+        aux = hangar.removeShieldBooster(i);
+        shieldBoosters.add(aux);
     }
     
     /**
-    * @brief builds a new WeaponToUI object from *this
-    * @return WeaponToUI
+    * @brief It trys to mount the weapon with the index i inside the hangar. If 
+    * a hangar is available, it removes the weapon located in that position 
+    * and if this operation is successful, it is added to the collection of
+    * weapons in use.
+    * @param i index
     */
-    void mountWeapons (int i) {
-        
+    void mountWeapon (int i) {
+        Weapon aux;
+        aux = hangar.removeWeapon(i);
+        weapons.add(aux);
     }
     
     /**
