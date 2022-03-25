bf3e78460ea548a38b5d28d1eea8af82150b54a4
Soledad Ruiz <solerugo@hotmail.com>
2022-03-25 12:10:47 +0100

    SS unfinished

--- Deepspace_Ruby/P2/SpaceStation.rb
+++ Deepspace_Ruby/P2/SpaceStation.rb
@@ -33,41 +33,99 @@ module DeepSpace
 
         #otro
 
+        # Set the amount of fuel to the value passed without ever 
+        # exceeding the limit.
         def assignFuelValue (f)
-            
+            if (f<=@@MAXFUEL)
+                @fuelUnits = f
+            else
+                @fuelUnits = @@MAXFUEL
+            end
         end
 
+        # Set pending damage to null if it has no effect.
         def cleanPendingDamage ()
+            if (pendingDamage.hasNoEffect)
+                pendingDamage = nil
         end
 
-        def cleanUpMountedItems ()
+        # It eliminates all mounted weapons and boosters that do not have 
+        # any use left.
+        def cleanUpMountedItems ()       
+           
+            @weapons.each do |aux|
+                if(aux.uses == 0)
+                    @weapons.delete(aux)
+                end
+            end
+
+            @shieldBoosters.each do |aux|
+                if(aux.uses == 0)
+                    @shieldBoosters.delete(aux)
+                end
+            end
+  
         end
 
+        # Set the reference of the hangar to null to indicate that it 
+        # is not available
         def discardHangar ()
+            @hangar = nil
         end
 
         def discardShieldBooster (i)
+            # next session
         end
 
+        # If a hangar is available, it is requested to discard the booster 
+        # with index i
         def discardShieldBoosterInHangar (i)
+            if (@hangar != nil)
+                @hangar.removeShieldBooster(i)
+            end
         end
 
+        # ---
         def discardWeapon (i)
+            # next session
         end
 
+        # If a hangar is available, it is requested to discard the weapon 
+        # with index i.
         def discardWeaponInHangar (i)
+            if (@hangar != nil)
+                @hangar.removeWeapon(i)
+            end
         end
 
+        # --
         def fire
+            # next session
+        end
+
+        # calculates the speed of the station
+         def speed
+            @fuelUnits/@@MAXFUEL
         end
 
         # builds a new WeaponToUI object from self
-        def UIVersion
+        def getUIVersion
             SpaceStationToUI.new(self)
         end
 
+        # It tries to mount the booster with the index i inside the hangar. If 
+        # a hangar is available, it removes the booster located in that position 
+        # and if this operation is successful, it is added to the collection of
+        # boosters in use.
         def mountShieldBooster (i)
+            if (@hangar != nil)
+                aux = @hangar.receiveShieldBooster(i)
+                if (aux != nil)
+                    @shieldBoosters.add(aux)
+                end 
+            end
         end
+
 
         #It trys to mount the weapon with the index i inside the hangar. If 
         #a hangar is available, it removes the weapon located in that position 
