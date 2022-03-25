970c242926991c41a2fbd872424d449a5b426d31
Soledad Ruiz <solerugo@hotmail.com>
2022-03-25 12:02:54 +0100

    SS unfinished

--- Deepspace_Ruby/P2/SpaceStation.rb
+++ Deepspace_Ruby/P2/SpaceStation.rb
@@ -9,16 +9,23 @@ module DeepSpace
 
     class SpaceStation
 
-        attr_reader :ammoPower, :hangar, :name, :nMedals, :pendingDamage, :shieldBoosters, :shieldPower, :speed, :weapons
+        attr_reader :ammoPower, :fuelUnits, :hangar, :name, :nMedals, :pendingDamage, :shieldBoosters, :shieldPower, :weapons
 
         @@MAXFUEL = 100
         @@SHIELDLOSSPERUNITSHOT = 0.1
         
         # Constructores HACER
-        def initialize (w, s)
-            @nShields = s 
-            @nWeapons = w
-            @weapons=Array.new()
+        def initialize (n, s)
+            @ammoPower = s.ammoPower
+            @fuelUnits = s.fuelUnits
+            @shieldPower = s.shieldPower
+            @name = n 
+            @nMedals = 0
+            @pendingDamage = nil
+            @hangar = nil
+            @weapons = Array.new
+            @shieldBoosters = Array.new 
+
         end
 
         # otro
@@ -26,35 +33,79 @@ module DeepSpace
 
         #otro
 
+        # Set the amount of fuel to the value passed without ever 
+        # exceeding the limit.
         def assignFuelValue (f)
-            
+            if (f<=@@MAXFUEL)
+                @fuelUnits = f
+            else
+                @fuelUnits = @@MAXFUEL
+            end
         end
 
+        # Set pending damage to null if it has no effect.
         def cleanPendingDamage ()
+            if (pendingDamage.hasNoEffect)
+                pendingDamage = nil
         end
 
-        def cleanUpMountedItems ()
+        # It eliminates all mounted weapons and boosters that do not have 
+        # any use left.
+        def cleanUpMountedItems ()       
+           
+            @weapons.each do |aux|
+                if(aux.uses == 0)
+                    @weapons.delete(aux)
+                end
+            end
+
+            @shieldBoosters.each do |aux|
+                if(aux.uses == 0)
+                    @shieldBoosters.delete(aux)
+                end
+            end
+  
         end
 
+        # Set the reference of the hangar to null to indicate that it 
+        # is not available
         def discardHangar ()
+            @hangar = nil
         end
 
         def discardShieldBooster (i)
+            # next session
         end
 
+        # If a hangar is available, it is requested to discard the booster 
+        # with index i
         def discardShieldBoosterInHangar (i)
+            if (@hangar != nil)
+                @hangar.removeShieldBooster(i)
+            end
         end
 
+        # ---
         def discardWeapon (i)
+            # next session
         end
 
+        # If a hangar is available, it is requested to discard the weapon 
+        # with index i.
         def discardWeaponInHangar (i)
+            if (@hangar != nil)
+                @hangar.removeWeapon(i)
+            end
         end
 
+        # --
         def fire
+            # next session
         end
-        
-        def getAmmoPower
+
+        # calculates the speed of the station
+         def speed
+            @fuelUnits/@@MAXFUEL
         end
 
         # builds a new WeaponToUI object from self
@@ -62,7 +113,17 @@ module DeepSpace
             SpaceStationToUI.new(self)
         end
 
+        # It tries to mount the booster with the index i inside the hangar. If 
+        # a hangar is available, it removes the booster located in that position 
+        # and if this operation is successful, it is added to the collection of
+        # boosters in use.
         def mountShieldBooster (i)
+            if (@hangar != nil)
+                aux = @hangar.receiveShieldBooster(i)
+                if (aux != nil)
+                    @shieldBoosters.add(aux)
+                end 
+            end
         end
 
         def mountWeapon (i)
