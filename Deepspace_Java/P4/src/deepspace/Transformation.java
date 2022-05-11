c23bae6041e38aee61c383a6d1532c586b39539a
clarabolivar <cbolivarpelaez@gmail.com>
2022-05-11 18:16:04 +0200

    .

---
+++ Deepspace_Java/P4/src/deepspace/Transformation.java
@@ -0,0 +1,12 @@
+/**
+ * @author BOLÍVAR PELÁEZ, CLARA
+ * @author RUIZ GÓMEZ, SOLEDAD
+ * 2º DGIIM - PDOO - Practice 4 DeepSpace
+ */
+package deepspace;
+
+/**
+ *
+ * @author clara
+ */
+public enum Transformation { NOTRANSFORM,GETEFFICIENT, SPACECITY }
